dd8226f158ee8655754802bee37641b8a01f7cf4
David Trussler <mail@davidtrussler.net>
2020-11-03 17:40:53 +0000

    Adds Storage Access API (WIP)

--- content-page/cookie.js
+++ content-page/cookie.js
@@ -7,5 +7,52 @@ const setCookie = () => {
 
 	document.cookie = `user=david; SameSite=None; secure`;
 
-	console.log('cookie: ', document.cookie); 
+	queryAccess(); 
+}
+
+const queryAccess = () => {
+	console.log('queryAccess!'); 
+
+	if (document.hasStorageAccess === undefined) {
+		return; 
+	} else {
+		document.hasStorageAccess().then(
+			function(hasAccess) {
+				console.log('hasAccess: ', hasAccess); 
+
+				if (hasAccess) {
+					return; 
+				} else {
+					// request access
+					console.log('You need to request access!'); 
+
+					// showPrompt(); 
+					
+					window.confirm('Do you wanna continue?', () => {
+					// 	promptAccepted(); 
+					// }
+
+					// function promptAccepted() {
+						document.requestStorageAccess().then(
+							() => {
+								console.log('user says yes!'); 								
+							}
+						).catch(e => {
+								console.log('user says no!');
+						})
+				// 	}
+					})
+				}
+			// function(reason) {
+			// 	console.log('reason: ', reason); 
+			}
+		// );
+		)
+	}
+}
+
+const showPrompt = () => {
+	if (window.confirm('Do you wanna continue?')) {
+		promptAccepted(); 
+	}
 }
